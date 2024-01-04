6d5f19c56dbcca44fb5e213101f452110d62310c
anshuljaiswal1998 <anshuljaiswal1998@gmail.com>
2024-01-05 01:52:19 +0530

    Added Tailwind

--- spotify-frontend/src/App.js
+++ spotify-frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<div>Hello</div>} ></Route>
+          <Route exact path='/hi' element={<div className="text-3xl font-bold underline">Hi</div>} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
